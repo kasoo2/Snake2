416ce9323f4ec69f87475c4aed5d94332be7dc29
kasoo2 <NikTrubin397sp@mail.ru>
2017-03-22 18:13:47 +0400

    Добавили класс Snake и Direction

--- Snake2/Snake2/Program.cs
+++ Snake2/Snake2/Program.cs
@@ -11,17 +11,20 @@ namespace Snake2
         static void Main(string[] args)
         {
 
-            HorizontallLine lineL = new HorizontallLine(5, 10, 5, '+');
-            lineL.Drow();
+            //HorizontallLine lineL = new HorizontallLine(5, 10, 5, '+');
+            //lineL.Drow();
 
-            HorizontallLine lineR = new HorizontallLine(5, 10, 20, '+');
-            lineR.Drow();
+            //HorizontallLine lineR = new HorizontallLine(5, 10, 20, '+');
+            //lineR.Drow();
 
-            VerticallLine lineUp = new VerticallLine(5, 20, 5, '+');
-            lineUp.Drow();
+            //VerticallLine lineUp = new VerticallLine(5, 20, 5, '+');
+            //lineUp.Drow();
 
-            VerticallLine lineDown = new VerticallLine(5, 20, 10, '+');
-            lineDown.Drow();
+            //VerticallLine lineDown = new VerticallLine(5, 20, 10, '+');
+            //lineDown.Drow();
+            Point p = new Point(4,5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drow();
 
             Console.ReadLine();
         }
